5e284224022c861f317fe4525a56579cf47d9706
Your Name <you@example.com>
2018-04-18 11:50:14 +0100

    final

--- src/main/java/account/accounts/App.java
+++ src/main/java/account/accounts/App.java
@@ -11,28 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        
-        Map<String, String>accountManager = new HashMap<String, String>(10, 0.5f);
-		//Adding employee name and salary to map
-//        accountManager.put();
-//        accountManager.put();
-//        accountManager.put();
-//        accountManager.put();
-//        accountManager.put();
-//        accountManager.put(); 
-//        
-		
-		System.out.println("Original Map: "+ accountManager);
-//		accountManager.put();
-//		accountManager.remove("Nidesh");
-		System.out.println("Updated Map: "+accountManager);
-		System.out.println(accountManager.keySet());
-		System.out.println(accountManager.values());
-	}
+      service application = new service();
+      
+      application.addAccounts("Fahim","Mulla","567");
+      
+      
+      application.newMap();
     }
 
-
+}
 
 	
 		
